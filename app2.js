bb4e40ee179d0f884c8a7f2cebe72d5016d2eaf7
Eli Dery <elidery@MacBook-Pro-3.local>
2018-02-09 14:45:28 -0700

    now it sorta works

--- app2.js
+++ app2.js
@@ -8,11 +8,23 @@ $(document).ready(function(){
       'date': dueDate
     }
     var taskHTML = "<tr><td>" + taskName + "</td><td>" + dueDate +"</td> \
-    <td><button class='btn btn-primary deleteTodo'>Done</button></td> \
+    <td><button id = 'debutton' class='btn btn-primary deleteTodo'>Done</button></td> \
   </tr>";
       $("#todoTable").append(taskHTML)
-    console.log(task)
+    console.log(task);
+    $(".deleteTodo").on('click', function(){
+      console.log($(this).parent().parent().children())
+      var aardvark = $(this).parent().parent();
+      aardvark = aardvark.children().slice(1,0);
+      $("#todonesList").prepend(aardvark)
+    });
   });
 });
 
+
+// $(document).ready(function(){
+//   $("#debutton").on('click', function(){
+//     console.log("Class Works :)")
+//   });
+// });
 // "<td>" + check +"</td>"
